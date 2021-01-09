dc57301139a084fcf9eacff040577d4e15ed7994
Lasse Karstensen <lasse@isokron.no>
2021-01-09 13:13:59 +0100

    Rename event function.

--- src/vmod_hydrogen.c
+++ src/vmod_hydrogen.c
@@ -20,7 +20,7 @@
 
 
 int v_matchproto_(vmod_event_f)
-vmod_event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+vmod_event(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
 {
     (void)ctx;
     (void)priv;
